1cca4a0df7bb7fbaa7a4c9015b6199209a62336d
permoK <perminusk21@gmail.com>
2025-01-07 22:24:04 +0300

    url modification

--- Auth/urls.py
+++ Auth/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include("RestApi.urls")),
+    path('chamaa/api/auth/', include("RestApi.urls")),
 ]
